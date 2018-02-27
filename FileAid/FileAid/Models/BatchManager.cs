047bb24986b3a172f07d11ec33dd33058306d941
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:16:47 -0500

    Adds BatchManager class stub

---
+++ FileAid/FileAid/Models/BatchManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public class BatchManager {
+        public static void Scan() {
+            // stub
+        }
+
+        private static List<string> GetActiveFiles() {
+            // stub
+        }
+
+        private static List<string> BuildLocationList(List<string> filenames) {
+            // stub
+        }
+
+        private static void ScanForKnown() {
+            // stub
+        }
+
+        private static bool CompareFileInfo(TrackedFile tf) {
+            // stub
+        }
+
+        private static void QueueUpdatedInfo(FileInfo fi) {
+            // stub
+        }
+
+        private static void ScanForNew() {
+            // stub
+        }
+
+        private static void QueueNewFile(TrackedFile newFile) {
+            // stub
+        }
+
+        public static BatchSummary Update() {
+            // stub
+        }
+
+        public static void LogSummary(BatchSummary summary) {
+            // stub
+        }
+    }
+}
