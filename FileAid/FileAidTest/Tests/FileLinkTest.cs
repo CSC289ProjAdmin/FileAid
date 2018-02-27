f3a9949751944391e6ec7dfe56cc072943d45b45
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:47:10 -0500

    Adds FileLink test stubs

---
+++ FileAid/FileAidTest/Tests/FileLinkTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class FileLinkTest {
+        [TestMethod]
+        public void GetFilesTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void JoinTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void UpdateMemoTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void RemoveMemoTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void RemoveFilesTest() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
