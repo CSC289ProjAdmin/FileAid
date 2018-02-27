f0ba7fc427c690c8fddda1c3c2f48ee06846999b
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:49:57 -0500

    Adds LinkManager test stubs

---
+++ FileAid/FileAidTest/Tests/LinkManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class LinkManagerTest {
+        [TestMethod]
+        public void GetLinksTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void GetLinkTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void AddLinkTest() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
