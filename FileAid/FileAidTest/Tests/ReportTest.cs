fdbbc391de3108520f74bca381938d273a411645
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:57:10 -0500

    Adds Report test stubs

---
+++ FileAid/FileAidTest/Tests/ReportTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class ReportTest {
+        [TestMethod]
+        public void Test() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
