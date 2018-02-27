3b6ffc6173fa502b8720b6de8fbbbeecb8ecdac8
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:55:05 -0500

    Adds Reminder test stubs

---
+++ FileAid/FileAidTest/Tests/ReminderTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class ReminderTest {
+        [TestMethod]
+        public void GetFilesTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void RemoveFilesTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void ResolveTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void PushTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void UpdateMemoTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void RemoveMemoTest() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
