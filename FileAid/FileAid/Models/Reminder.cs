2c14a2782db2636baa8fa52c27a2d1c61a4bfd63
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:09:09 -0500

    Adds Reminder class stub

---
+++ FileAid/FileAid/Models/Reminder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public class Reminder {
+        public List<TrackedFile> GetFiles() {
+            // stub
+        }
+
+        public void RemoveFiles(List<TrackedFile> exMembers) {
+            // stub
+        }
+
+        public void Resolve() {
+            // stub
+        }
+
+        public void Push() {
+            // stub
+        }
+
+        public void UpdateMemo(string memo) {
+            // stub
+        }
+
+        public void RemoveMemo() {
+            // stub
+        }
+    }
+}
