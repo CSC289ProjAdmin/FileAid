8178970c56b112aa39e83418d80769fc6072d617
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:48:41 -0500

    Adds FileManager test stubs

---
+++ FileAid/FileAidTest/Tests/FileManagerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class FileManagerTest {
+        [TestMethod]
+        public void GetFilesTest() {
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void AddFilesTest() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
