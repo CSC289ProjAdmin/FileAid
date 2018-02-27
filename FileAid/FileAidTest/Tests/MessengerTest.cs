e8dc0785e4026d73b98f695aa4ac330010683285
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:52:02 -0500

    Adds Messenger test stubs

---
+++ FileAid/FileAidTest/Tests/MessengerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class MessengerTest {
+        [TestMethod]
+        public void ShowTest() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
