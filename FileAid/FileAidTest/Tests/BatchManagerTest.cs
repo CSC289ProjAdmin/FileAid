9a16eb1db0923ef26d7a178aea8007955686f6dd
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:42:01 -0500

    Adds BatchManager test stubs

---
+++ FileAid/FileAidTest/Tests/BatchManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class BatchManagerTest {
+        [TestMethod]
+        public void ScanTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void UpdateTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void LogSummaryTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+    }
+}
