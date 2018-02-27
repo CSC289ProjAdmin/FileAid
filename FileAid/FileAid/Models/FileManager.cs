26869f71c039322352d3a70afe9e3e7b7e114499
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 21:57:39 -0500

    Adds FileManager class stub

---
+++ FileAid/FileAid/Models/FileManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public class FileManager {
+        public static List<TrackedFile> GetFiles() {
+            // stub
+        }
+
+        public static void AddFiles(List<string> filenames) {
+            // stub
+        }
+    }
+}
