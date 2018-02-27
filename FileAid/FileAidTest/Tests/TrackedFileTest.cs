860ae1040f3fe0dc8a420b70943d710bc31253de
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:59:01 -0500

    Adds TrackedFile test stubs

---
+++ FileAid/FileAidTest/Tests/TrackedFileTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class TrackedFileTest {
+        [TestMethod]
+        public void GetInfoTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void GetLinksTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void GetReminderTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void UpdateMemoTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void RemoveMemoTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void GetHistoryTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void StopTrackingTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void StartTrackingTest() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
