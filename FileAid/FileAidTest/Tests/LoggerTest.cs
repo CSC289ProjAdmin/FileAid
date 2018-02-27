d2f7f99b778045e58850209386c495046284bba9
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:50:59 -0500

    Adds Logger test stubs

---
+++ FileAid/FileAidTest/Tests/LoggerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests {
+    [TestClass]
+    public class LoggerTest {
+        [TestMethod]
+        public void LogTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void LogToDbTest() {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void LogToFileTest() {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
