fcb99edcbdb6a1a8b174d1e36ea05a2dc641a0e2
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:06:16 -0500

    Adds LinkManager class stub

---
+++ FileAid/FileAid/Models/LinkManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public class LinkManager {
+        public static List<FileLink> GetLinks() {
+            // stub
+        }
+
+        public static FileLink GetLink(int linkMemoID) {
+            // stub
+        }
+
+        public static void AddLink(List<TrackedFile> newGroup) {
+            // stub
+        }
+    }
+}
