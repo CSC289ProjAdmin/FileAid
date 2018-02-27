78336386593dd297e671775319ef82c904a60905
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-26 22:27:39 -0500

    Adds Logger class stub

---
+++ FileAid/FileAid/Models/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public class Logger {
+        public static void Log(Event e) {
+            // stub
+        }
+
+        public static void LogToDb(Event e) {
+            // stub
+        }
+
+        public static void LogToFile(Event e, string logfile) {
+            // stub
+        }
+    }
+}
