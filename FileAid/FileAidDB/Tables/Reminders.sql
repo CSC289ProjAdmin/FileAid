8188ad5175a6d2afb7a5d1dd8749c12074ddfc15
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-03 08:39:41 -0500

    Adds three tables to database

---
+++ FileAid/FileAidDB/Tables/Reminders.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Reminders]
+(
+	ReminderID INT NOT NULL PRIMARY KEY Identity,
+	sReminderName varchar(100) not null,
+	dDue datetime not null,
+	sReminderMemo varchar(100),
+	dResolved datetime,
+	dPushed datetime,
+	dReminderCreated datetime not null,
+	dReminderUpdated datetime not null,
+	dReminderDeleted datetime
+)
