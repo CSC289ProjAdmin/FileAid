bf394f69ef4edb8dcbc7a162ea0c1e69179e41d8
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-03 08:56:17 -0500

    Adds four tables to database

---
+++ FileAid/FileAidDB/Tables/Roles.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Roles]
+(
+	RoleID INT NOT NULL PRIMARY KEY Identity,
+	sRoleName varchar(100) not null,
+	PermID int not null References Permissions(PermID),
+	dRoleCreated datetime not null,
+	dRoleUpdated datetime not null,
+	dRoleDeleted datetime
+)
---
+++ FileAid/FileAidDB/Tables/Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	UserID INT NOT NULL PRIMARY KEY Identity,
+	sUserName varchar(100) not null,
+	sPassword varchar(100) not null,
+	RoleID int not null References Roles(RoleID),
+	iFailures int not null default 0,
+	dLockedOut datetime,
+	dUserDisabled datetime,
+	dUserCreated datetime not null,
+	dUserUpdated datetime not null,
+	dUserDeleted datetime
+)
