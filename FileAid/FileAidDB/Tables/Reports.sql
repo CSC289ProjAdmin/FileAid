ff1e82d72b42ce5692e0b44d4c436d3521763bb0
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-03 08:02:51 -0500

    Adds first table and publish profile

---
+++ FileAid/FileAidDB/Tables/Reports.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Reports]
+(
+	ReportID INT NOT NULL PRIMARY KEY Identity,
+	sReportName varchar(100) not null,
+	sReportDescription varchar(100) not null,
+	dReportCreated datetime not null,
+	dReportUpdated datetime not null,
+	dReportDeleted datetime
+)
