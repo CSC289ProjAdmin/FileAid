ea2727a07952ce123e25dffa1f1635c791dd4475
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-04 15:11:32 -0500

    Implements FileLinkDAL.GetFiles to return all files in a given link group

--- FileAid/FileAid/DAL/FileLinkDAL.cs
+++ FileAid/FileAid/DAL/FileLinkDAL.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FileAid.Models;
+using System.Data.SqlClient;
 
 namespace FileAid.DAL {
     public static class FileLinkDAL {
-        public static List<TrackedFile> GetFiles() {
-            // stub
-            return Db.ReadQuery<TrackedFile>("stub");
+        public static List<TrackedFile> GetFiles(int linkID) {
+            string select = "Select FileID, sFileName As Filename, sFileExt As FileExtension, " +
+                "sFilePath As FilePath, iSizeInBytes As FileSize, dFileModified As ModifiedOn, " +
+                "dFileCreated As CreatedOn, sFileMemo As FileMemo, " +
+                "dTrackingDisabled As TrackingDisabledOn, ReminderID From TrackedFiles " +
+                "Inner Join FileLinks On TrackedFiles.FileID = FileLinks.FileID " +
+                "Where dTrackDeleted Is Null " +
+                "And dLinkDeleted Is Null And LinkID = @LinkID ";
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@LinkID", linkID));
+            return Db.ReadQuery<TrackedFile>(select, args.ToArray());
         }
 
         public static void Join(int memberFileID) {
