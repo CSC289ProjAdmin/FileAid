82eb1dfe3aa3bf737b1e1c420f1df95aafc5dd9e
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-04 12:32:11 -0500

    Adds properties to Report class

--- FileAid/FileAid/Models/Report.cs
+++ FileAid/FileAid/Models/Report.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace FileAid.Models {
     public class Report {
+        public int ReportID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         // stub
     }
 }
