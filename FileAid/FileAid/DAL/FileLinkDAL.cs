e754e360bb91851a9776582f35fc0c88ec917577
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-04 16:30:49 -0500

    Adds comments to FileLinkDAL.Join

--- FileAid/FileAid/DAL/FileLinkDAL.cs
+++ FileAid/FileAid/DAL/FileLinkDAL.cs
@@ -10,6 +10,7 @@ namespace FileAid.DAL {
     public static class FileLinkDAL {
         public static List<TrackedFile> GetFiles(int linkMemoID) {
             if (linkMemoID <= 0) return null; // not required but prevents an unnecessary db call
+            // Get list of all files belonging to this link/group
             string select = "Select TrackedFiles.FileID, sFileName As Filename, sFileExt As FileExtension, " +
                 "sFilePath As FilePath, iSizeInBytes As FileSize, dFileModified As ModifiedOn, " +
                 "dFileCreated As CreatedOn, sFileMemo As FileMemo, " +
@@ -27,19 +28,16 @@ namespace FileAid.DAL {
             List<SqlParameter> args = new List<SqlParameter>();
             args.Add(new SqlParameter("@FileID", memberFileID));
             args.Add(new SqlParameter("@LinkMemoID", linkMemoID));
-
-            // Prevent a file from being added to a group it is already in
+            // Prevent a file from being added to any group it is already in
             string select = "Select Count(*) As ExistingLinks From FileLinks " +
                 "Where FileID = @FileID And LinkMemoID = @LinkMemoID And dLinkDeleted Is Null";
             bool alreadyInGroup = ((int)Db.ExecuteScalar(select, args.ToArray()) > 0);
             if (!alreadyInGroup) {
-                string insert = "Insert Into FileLinks (FileID, LinkMemoID, dLinkCreated, dLinkUpdated) "+
+                // Add file to the group
+                string insert = "Insert Into FileLinks (FileID, LinkMemoID, dLinkCreated, dLinkUpdated) " +
                     "Values (@FileID, @LinkMemoID, GetDate(), GetDate());" +
                     "Select Convert(int, Scope_Identity());";
                 int newID = (int)Db.ExecuteScalar(insert, args.ToArray());
-                if (!(newID > 0)) {
-                    throw new Exception();
-                }
             }
         }
 
