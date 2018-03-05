102b8321cc6bdd751e58f53429a9d1eedc429d4a
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-04 19:44:42 -0500

    Updates the AddLink overload that takes a memo string to add the list of files to the new group

--- FileAid/FileAid/DAL/LinkManagerDAL.cs
+++ FileAid/FileAid/DAL/LinkManagerDAL.cs
@@ -36,6 +36,9 @@ namespace FileAid.DAL {
                 "Select Convert(int, Scope_Identity());";
             int newID = (int)Db.ExecuteScalar(insert, args.ToArray());
             // Add list of files to the group
+            foreach (int id in fileIDs) {
+                FileLinkDAL.Join(newID, id);
+            }
         }
 
         public static void AddLink(List<int> fileIDs) {
@@ -45,19 +48,6 @@ namespace FileAid.DAL {
                 "Select Convert(int, Scope_Identity());";
             int newID = (int)Db.ExecuteScalar(insert);
             // Add list of files to the group
-/*
-            int fileID = -999;
-            List<SqlParameter> args = new List<SqlParameter>();
-            args.Add(new SqlParameter("@FileID", fileID));
-            args.Add(new SqlParameter("@LinkMemoID", newID));
-            string joinGroup = "Insert Into FileLinks (FileID, LinkMemoID, dLinkCreated, dLinkUpdated " +
-                "Values (@FileID, @LinkMemoID, GetDate(), GetDate()); " +
-                "Select Convert(int, Scope_Identity());";
-            foreach (int id in fileIDs) {
-                args[0].Value = id;
-                int newLinkID = (int)Db.ExecuteScalar(joinGroup, args.ToArray());
-            }
-*/
             foreach (int id in fileIDs) {
                 FileLinkDAL.Join(newID, id);
             }
