414bc75e6ecd51eab3a29838fb3e92f79467c764
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-05 16:19:10 -0500

    Removes password property from User class

--- FileAid/FileAid/Models/User.cs
+++ FileAid/FileAid/Models/User.cs
@@ -8,7 +8,7 @@ namespace FileAid.Models {
     public class User {
         public int UserID { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
+        //public string Password { get; set; } // Is this needed in the User object?
         public int RoleID { get; set; }
         public int LoginFailures { get; set; }
         public DateTime? LockedOutOn { get; set; } = null;
