79cafe1ea5ad32a328c0581804d982f802e07e91
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-05 13:17:27 -0500

    Changes TrackedFile property to non-nullable until it is handled in Reflection code

--- FileAid/FileAid/DAL/FileManagerDAL.cs
+++ FileAid/FileAid/DAL/FileManagerDAL.cs
@@ -12,6 +12,7 @@ namespace FileAid.DAL {
                 "iSizeInBytes As FileSize, dFileModified As ModifiedOn, dFileCreated As CreatedOn, sFileMemo As FileMemo, " +
                 "dTrackingDisabled As TrackingDisabledOn, ReminderID " +
                 "From TrackedFiles Where dTrackDeleted Is Null";
+            //! TODO: Handle nullable DateTime
             return Db.ReadQuery<TrackedFile>(select);
         }
 
