db7ecf00f852cb075ef3a69593c0e913f2786df1
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-04 20:17:28 -0500

    Implements ReminderDAL.GetFiles to retrieve list of files associated with a reminder

--- FileAid/FileAid/DAL/ReminderDAL.cs
+++ FileAid/FileAid/DAL/ReminderDAL.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FileAid.Models;
+using System.Data.SqlClient;
 
 namespace FileAid.DAL {
     public static class ReminderDAL {
-        public static List<TrackedFile> GetFiles() {
-            // stub
-            return Db.ReadQuery<TrackedFile>("stub");
+        public static List<TrackedFile> GetFiles(int reminderID) {
+            if (reminderID <= 0) return null; // not required but prevents an unnecessary db call
+            // Get list of all files belonging to this Reminder
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@ReminderID", reminderID));
+            string select = "Select FileID, sFileName As Filename, sFileExt As FileExtension, " +
+                "sFilePath As FilePath, iSizeInBytes As FileSize, dFileModified As ModifiedOn, " +
+                "dFileCreated As CreatedOn, sFileMemo As FileMemo, " +
+                "dTrackingDisabled As TrackingDisabledOn, TrackedFiles.ReminderID From TrackedFiles " +
+                "Inner Join Reminders On TrackedFiles.ReminderID = Reminders.ReminderID " +
+                "Where dTrackDeleted Is Null " +
+                "And dReminderDeleted Is Null And TrackedFiles.ReminderID = @ReminderID ";
+            return Db.ReadQuery<TrackedFile>(select, args.ToArray());
         }
 
         public static void RemoveFiles(List<int> exMemberFileIDs) {
