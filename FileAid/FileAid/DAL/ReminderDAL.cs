44bf1bec962c2266a2098e94f164f3212362650b
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-04 21:13:02 -0500

    Implements UpdateMemo to set new reminder memo/description

--- FileAid/FileAid/DAL/ReminderDAL.cs
+++ FileAid/FileAid/DAL/ReminderDAL.cs
@@ -67,12 +67,39 @@ namespace FileAid.DAL {
             int modifiedRows = (int)Db.ExecuteNonQuery(update, args.ToArray());
         }
 
-        public static void UpdateMemo(string newMemo) {
-            // stub
+        public static void UpdateMemo(int reminderID, string newMemo) {
+            if (reminderID <= 0) return; // not required but prevents an unnecessary db call
+            List<SqlParameter>args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@ReminderID", reminderID));
+            args.Add(new SqlParameter("@Memo", newMemo));
+            string update = "Update Reminders Set sReminderMemo = @Memo, dReminderUpdated = GetDate() " +
+                "Where ReminderID = @ReminderID And dReminderDeleted Is Null";
+            int modifiedRows = (int)Db.ExecuteNonQuery(update, args.ToArray());
         }
 
         public static void RemoveMemo() {
             // stub
         }
+/*
+        public static void UpdateMemo(int linkMemoID, string newMemo) {
+            if (linkMemoID <= 0) return; // not required but prevents an unnecessary db call
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@LinkMemoID", linkMemoID));
+            args.Add(new SqlParameter("@Memo", newMemo));
+            string update = "Update LinkMemos Set sLinkMemo = @Memo, dMemoUpdated = GetDate() " +
+                "Where LinkMemoID = @LinkMemoID And dMemoDeleted Is Null";
+            int modifiedRows = (int)Db.ExecuteNonQuery(update, args.ToArray());
+        }
+
+        public static void RemoveMemo(int linkMemoID) {
+            if (linkMemoID <= 0) return; // not required but prevents an unnecessary db call
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@LinkMemoID", linkMemoID));
+            string update = "Update LinkMemos Set sLinkMemo = null, dMemoUpdated = GetDate() " +
+                "Where LinkMemoID = @LinkMemoID And dMemoDeleted Is Null";
+            int modifiedRows = (int)Db.ExecuteNonQuery(update, args.ToArray());
+        }
+
+*/
     }
 }
