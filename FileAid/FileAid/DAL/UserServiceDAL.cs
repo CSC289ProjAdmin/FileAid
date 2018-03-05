8d4d3d89f63914228f5d2ae01410f89f103c4b5f
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-05 14:37:03 -0500

    Implements Enable method to give user access to program

--- FileAid/FileAid/DAL/UserServiceDAL.cs
+++ FileAid/FileAid/DAL/UserServiceDAL.cs
@@ -71,8 +71,14 @@ namespace FileAid.DAL {
             int modifiedRows = (int)Db.ExecuteNonQuery(update, args.ToArray());
         }
 
-        public static void Enable(User u) {
-            // stub
+        public static void Enable(int userID) {
+            if (userID <= 0) return; // not required but prevents an unnecessary db call
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@UserID", userID));
+            // Only clear disable date if it exists (is not null)
+            string update = "Update Users Set dUserDisabled = null, dUserUpdated = GetDate() " +
+                "Where UserID = @UserID And dUserDeleted Is Null and dUserDisabled Is Not Null;";
+            int modifiedRows = (int)Db.ExecuteNonQuery(update, args.ToArray());
         }
 
         public static void ResetPassword(User u) {
