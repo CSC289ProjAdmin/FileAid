4aca3e59554ab69a3afaed48b6ef2285f322f117
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-04 21:38:27 -0500

    Implements ReminderManagerDAL.GetReminders to retrieve all active reminders

--- FileAid/FileAid/DAL/ReminderManagerDAL.cs
+++ FileAid/FileAid/DAL/ReminderManagerDAL.cs
@@ -7,8 +7,10 @@ using FileAid.Models;
 namespace FileAid.DAL {
     public static class ReminderManagerDAL {
         public static List<Reminder> GetReminders() {
-            // stub
-            return Db.ReadQuery<Reminder>("stub");
+            string select = "Select ReminderID, sReminderName As Name, dDue As DueOn, " +
+                "sReminderMemo As Memo, dResolved As ResolvedOn, dPushed As PushedOn " +
+                "From Reminders Where dReminderDeleted Is Null";
+            return Db.ReadQuery<Reminder>(select);
         }
 
         public static Reminder GetReminder(int reminderID) {
@@ -20,4 +22,58 @@ namespace FileAid.DAL {
             // stub
         }
     }
+/*
+        public int ReminderID { get; set; }
+        public string Name { get; set; }
+        public DateTime DueOn { get; set; }
+        public string Memo { get; set; } = null;
+        public DateTime? ResolvedOn { get; set; } = null;
+        public DateTime? PushedOn { get; set; } = null;
+
+
+        public static List<FileLink> GetLinks() {
+            string select = "Select LinkMemoID, sLinkMemo As LinkMemo From LinkMemos " +
+                "Where dMemoDeleted Is Null";
+            return Db.ReadQuery<FileLink>(select);
+        }
+
+        public static FileLink GetLink(int linkMemoID) {
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@LinkMemoID", linkMemoID));
+            string select = "Select LinkMemoID, sLinkMemo As LinkMemo From LinkMemos " +
+                "Where dMemoDeleted Is Null And LinkMemoID = @LinkMemoID";
+            List<FileLink> results = Db.ReadQuery<FileLink>(select, args.ToArray());
+            if (results != null) {
+                return results[0];
+            } else {
+                return null;
+            }
+        }
+
+        public static void AddLink(List<int> fileIDs, string linkMemo) {
+            // Create the link group
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@Memo", linkMemo));
+            string insert = "Insert Into LinkMemos (sLinkMemo, dMemoCreated, dMemoUpdated) " +
+                "Values (@Memo, GetDate(), GetDate()); " +
+                "Select Convert(int, Scope_Identity());";
+            int newID = (int)Db.ExecuteScalar(insert, args.ToArray());
+            // Add list of files to the group
+            foreach (int id in fileIDs) {
+                FileLinkDAL.Join(newID, id);
+            }
+        }
+
+        public static void AddLink(List<int> fileIDs) {
+            // Create the link group (without setting up a memo)
+            string insert = "Insert Into LinkMemos (dMemoCreated, dMemoUpdated) " +
+                "Values (GetDate(), GetDate()); " +
+                "Select Convert(int, Scope_Identity());";
+            int newID = (int)Db.ExecuteScalar(insert);
+            // Add list of files to the group
+            foreach (int id in fileIDs) {
+                FileLinkDAL.Join(newID, id);
+            }
+        }
+*/
 }
