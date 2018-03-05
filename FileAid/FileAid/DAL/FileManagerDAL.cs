156c3f6f699bc68386a37939b6f71b8eb8ee81fc
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-05 13:30:03 -0500

    Implements GetFile method to retrieve a specific file

--- FileAid/FileAid/DAL/FileManagerDAL.cs
+++ FileAid/FileAid/DAL/FileManagerDAL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FileAid.Models;
+using System.Data.SqlClient;
 
 namespace FileAid.DAL {
     public static class FileManagerDAL {
@@ -16,9 +17,20 @@ namespace FileAid.DAL {
             return Db.ReadQuery<TrackedFile>(select);
         }
 
-        public static TrackedFile GetFile() {
-            //stub
-            return new TrackedFile();
+        public static TrackedFile GetFile(int fileID) {
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@FileID", fileID));
+            string select = "Select FileID, sFileName As Filename, sFileExt As FileExtension, sFilePath As FilePath, " +
+                "iSizeInBytes As FileSize, dFileModified As ModifiedOn, dFileCreated As CreatedOn, sFileMemo As FileMemo, " +
+                "dTrackingDisabled As TrackingDisabledOn, ReminderID " +
+                "From TrackedFiles Where dTrackDeleted Is Null And FileID = @FileID;";
+            //! TODO: Handle nullable DateTime
+            List<TrackedFile> results = Db.ReadQuery<TrackedFile>(select, args.ToArray());
+            if (results != null) {
+                return results[0];
+            } else {
+                return null;
+            }
         }
 
         public static void AddFile(TrackedFile newFile) {
