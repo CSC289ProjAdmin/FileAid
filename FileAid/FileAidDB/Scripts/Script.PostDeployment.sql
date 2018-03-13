842195aa2a3b799374855ea57e8d15e012b6e0f4
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-12 21:45:36 -0400

    Adds testing data into post-deployment script to allow unit testing TrackedFile class

--- FileAid/FileAidDB/Scripts/Script.PostDeployment.sql
+++ FileAid/FileAidDB/Scripts/Script.PostDeployment.sql
@@ -14,3 +14,146 @@ Insert Into Reports
 Values
     ('Logins', 'Record of all logins to FileAid', GetDate(), GetDate())
 ;
+
+INSERT INTO [dbo].[EventTypes]
+           ([sTypeCategory]
+           ,[sTypeDescription]
+           ,[dTypeCreated]
+           ,[dTypeUpdated]
+           ,[dTypeDeleted])
+     VALUES
+           ('File Tracking'
+           ,'File Added'
+           ,getdate()
+           ,getdate()
+           ,null)
+;
+
+INSERT INTO [dbo].[Reminders]
+           ([sReminderName]
+           ,[dDue]
+           ,[sReminderMemo]
+           ,[dResolved]
+           ,[dPushed]
+           ,[dReminderCreated]
+           ,[dReminderUpdated]
+           ,[dReminderDeleted])
+     VALUES
+           ('First reminder'
+           ,'2018-12-25'
+           ,'Description for first reminder'
+           ,null
+           ,null
+           ,getdate()
+           ,getdate()
+           ,null)
+;
+
+
+INSERT INTO [dbo].[LinkMemos]
+           ([sLinkMemo]
+           ,[dMemoCreated]
+           ,[dMemoUpdated]
+           ,[dMemoDeleted])
+     VALUES
+           ('First link'
+           ,getdate()
+           ,getdate()
+           ,null)
+;
+
+
+INSERT INTO [dbo].[TrackedFiles]
+           ([sFileName]
+           ,[sFileExt]
+           ,[sFilePath]
+           ,[iSizeInBytes]
+           ,[dFileModified]
+           ,[dFileCreated]
+           ,[sFileMemo]
+           ,[dTrackingDisabled]
+           ,[ReminderID]
+           ,[dTrackCreated]
+           ,[dTrackUpdated]
+           ,[dTrackDeleted])
+     VALUES
+           ('First File'
+           ,'DOCX'
+           ,'C:\My Documents'
+           ,1000000
+           ,getdate()
+           ,getdate()
+           ,'Description of first file'
+           ,null
+           ,(select min(reminderID) from reminders where sReminderName like '%first%')
+           ,getdate()
+           ,getdate()
+           ,null)
+           ,
+           ('Second File'
+           ,'XLXS'
+           ,'C:\Users\Desktop'
+           ,50000
+           ,getdate()
+           ,getdate()
+           ,'Description of second file'
+           ,null
+           ,null
+           ,getdate()
+           ,getdate()
+           ,null)
+;
+
+
+INSERT INTO [dbo].[Events]
+           ([EventTypeID]
+           ,[dEvent]
+           ,[sEventDescription]
+           ,[sInitial]
+           ,[sNew]
+           ,[FileID]
+           ,[LinkID]
+           ,[ReportID]
+           ,[ReminderID]
+           ,[BatchID]
+           ,[UserID]
+           ,[LoginID]
+           ,[PermID]
+           ,[ConfigID]
+           ,[dEventCreated]
+           ,[dEventUpdated]
+           ,[dEventDeleted])
+     VALUES
+           ((select min(eventtypeid) from EventTypes where sTypeDescription like '%file added%')
+           ,getdate()
+           ,'First file added to system'
+           ,null
+           ,null
+           ,(select min(fileid) from TrackedFiles where sfilename like '%First%')
+           ,null
+           ,null
+           ,null
+           ,null
+           ,null
+           ,null
+           ,null
+           ,null
+           ,getdate()
+           ,getdate()
+           ,null)
+;
+
+
+INSERT INTO [dbo].[FileLinks]
+           ([FileID]
+           ,[LinkMemoID]
+           ,[dLinkCreated]
+           ,[dLinkUpdated]
+           ,[dLinkDeleted])
+     VALUES
+           ((select min(fileid) from TrackedFiles where sFileName like '%First%')
+           ,(select min(linkmemoID) from LinkMemos where sLinkMemo like '%First%')
+           ,getdate()
+           ,getdate()
+           ,null)
+;
