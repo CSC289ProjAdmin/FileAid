0aefd77214c4fcc93a4d53c3b9a1ea985acac9c1
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-15 21:59:05 -0400

    Implements FileManager BLL methods

--- FileAid/FileAid/Models/FileManager.cs
+++ FileAid/FileAid/Models/FileManager.cs
@@ -7,13 +7,27 @@ using System.Threading.Tasks;
 namespace FileAid.Models {
     public static class FileManager {
         public static List<TrackedFile> GetFiles() {
-            // stub
-            List<TrackedFile> dummy = new List<TrackedFile>();
-            return dummy;
+            List<TrackedFile> myFiles = DAL.FileManagerDAL.GetFiles();
+            return myFiles;
         }
 
-        public static void AddFiles(List<string> filenames) {
-            // stub
+        public static TrackedFile GetFile(int fileID) {
+            TrackedFile file = DAL.FileManagerDAL.GetFile(fileID);
+            return file;
+        }
+
+        public static bool AddFile(string filename, string extension, string path, int filesize,
+                DateTime created, DateTime modified, string memo) {
+            bool wasAdded = DAL.FileManagerDAL.AddFile(filename, extension, path, filesize,
+                created, modified, memo);
+            return wasAdded;
+        }
+
+        public static bool AddFile(string filename, string extension, string path, int filesize,
+                DateTime created, DateTime modified) {
+            bool wasAdded = DAL.FileManagerDAL.AddFile(filename, extension, path, filesize,
+                created, modified);
+            return wasAdded;
         }
     }
 }
