28eb4b1764d52262c8ae9801630b7f5e32e4b2a0
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-18 13:03:46 -0400

    Adds default password column to Users table for easy resetting of passwords

--- FileAid/FileAidDB/Tables/Users.sql
+++ FileAid/FileAidDB/Tables/Users.sql
@@ -3,6 +3,7 @@
 	UserID INT NOT NULL PRIMARY KEY Identity,
 	sUserName varchar(100) not null,
 	sPassword varchar(100) not null,
+	sDefaultPassword varchar(100) not null,
 	RoleID int not null References Roles(RoleID),
 	iFailures int not null default 0,
 	dLockedOut datetime,
