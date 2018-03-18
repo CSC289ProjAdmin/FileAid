881e255953563405b1bf325c331b21685452606b
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-18 15:16:24 -0400

    Adds EventManager BLL class

---
+++ FileAid/FileAid/Models/EventManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public static class EventManager {
+        public static List<Event> GetEvents() {
+            List<Event> allEvents = DAL.EventManagerDAL.GetEvents();
+            return allEvents;
+        }
+
+        public static Event GetEvent(int eventID) {
+            Event specific = DAL.EventManagerDAL.GetEvent(eventID);
+            return specific;
+        }
+    }
+}
