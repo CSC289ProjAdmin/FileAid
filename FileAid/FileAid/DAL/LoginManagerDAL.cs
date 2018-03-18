d24bed0e733bf231c4845a6e7d1686af8f8a9a85
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-18 15:40:35 -0400

    Adds LoginManager DAL class for retrieving / writing to Logins table

---
+++ FileAid/FileAid/DAL/LoginManagerDAL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileAid.Models;
+using System.Data.SqlClient;
+
+namespace FileAid.DAL {
+    public static class LoginManagerDAL {
+        public static List<Login> GetLogins() {
+            string select = "Select LoginID, UserID, dLogin As OccurredOn " +
+                "From Logins Where dLoginDeleted Is Null;";
+            return Db.ReadQuery<Login>(select);
+        }
+
+        public static Login GetLogin(int loginID) {
+            if (loginID <= 0) return null;
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@LoginID", loginID));
+            string select = "Select LoginID, UserID, dLogin As OccurredOn " +
+                "From Logins Where dLoginDeleted Is Null And LoginID = @LoginID;";
+            List<Login> results = Db.ReadQuery<Login>(select, args.ToArray());
+            if (results != null) {
+                return results[0];
+            } else {
+                return null;
+            }
+        }
+
+        public static int AddLogin(int userID) {
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@UserID", userID));
+            string insert = "Insert Into Logins (UserID, dLogin, dLoginCreated, dLoginUpdated) " +
+                "Values (@UserID, GetDate(), GetDate(), GetDate()); " +
+                "Select Convert(int, Scope_Identity());";
+            int newID = -999;
+            try {
+                newID = (int)Db.ExecuteScalar(insert, args.ToArray());
+            } catch (SqlException) {
+            }
+            return newID;
+        }
+    }
+}
---
+++ FileAid/FileAid/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public class Login {
+        public int LoginID { get; set; }
+        public int UserID { get; set; }
+        public DateTime OccurredOn { get; set; }
+    }
+}
