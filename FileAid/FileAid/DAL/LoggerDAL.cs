c3d4a574bc7a97b98ee4b31e82bad1ec4f8f6c81
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-18 13:57:40 -0400

    Adds LoggerDAL class for writing Events to database

---
+++ FileAid/FileAid/DAL/LoggerDAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileAid.Models;
+using System.Data.SqlClient;
+
+namespace FileAid.DAL {
+    public class LoggerDAL {
+        public static bool LogToDb(Event e) {
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@EventTypeID", e.EventTypeID));
+            args.Add(new SqlParameter("@EventDate", e.OccurredOn));
+            args.Add(new SqlParameter("@Description", e.Description));
+            args.Add(new SqlParameter("@Initial", e.Initial));
+            args.Add(new SqlParameter("@New", e.New));
+
+            args.Add(new SqlParameter("@FileID", (e.FileID != -999 ? e.FileID.ToString() : "null") ));
+            args.Add(new SqlParameter("@LinkID", (e.LinkID != -999 ? e.LinkID.ToString() : "null") ));
+            args.Add(new SqlParameter("@ReportID", (e.ReportID != -999 ? e.ReportID.ToString() : "null") ));
+            args.Add(new SqlParameter("@ReminderID", (e.ReminderID != -999 ? e.ReminderID.ToString() : "null") ));
+            args.Add(new SqlParameter("@BatchID", (e.BatchID != -999 ? e.BatchID.ToString() : "null") ));
+            args.Add(new SqlParameter("@UserID", (e.UserID != -999 ? e.UserID.ToString() : "null") ));
+            args.Add(new SqlParameter("@LoginID", (e.LoginID != -999 ? e.LoginID.ToString() : "null") ));
+            args.Add(new SqlParameter("@PermID", (e.PermID != -999 ? e.PermID.ToString() : "null") ));
+            args.Add(new SqlParameter("@ConfigID", (e.ConfigID != -999 ? e.ConfigID.ToString() : "null") ));
+
+            string insert = "Insert Into Events (EventTypeID, dEvent, sEventDescription, sInitial, sNew, " +
+                "FileID, LinkID, ReportID, ReminderID, BatchID, UserID, LoginID, PermID, ConfigID) " +
+                "Values (@EventTypeID, @EventDate, @Description, @Initial, @New, " +
+                "@FileID, @LinkID, @ReportID, @ReminderID, @BatchID, @UserID, @LoginID, @PermID, @ConfigID); " +
+                "Select Convert(int, Scope_Identity());";
+            int newID = (int)Db.ExecuteScalar(insert, args.ToArray());
+            bool wasAdded = (newID > 0);
+            return wasAdded;
+        }
+    }
+}
--- FileAid/FileAid/Models/Event.cs
+++ FileAid/FileAid/Models/Event.cs
@@ -6,23 +6,24 @@ using System.Threading.Tasks;
 
 namespace FileAid.Models {
     public class Event {
-        public int EventID { get; set; }
-        public int EventTypeID { get; set; }
+        public int EventID { get; set; } = -999;
+        public int EventTypeID { get; set; } = -999;
         public DateTime OccurredOn { get; set; }
-        public string Description { get; set; } = null;
-        public string Initial { get; set; } = null;
-        public string New { get; set; } = null;
+        public string Description { get; set; }
+        public string Initial { get; set; }
+        public string New { get; set; }
         /* Is it necessary to include these IDs in the object?
-        public int? FileID { get; set; } = null;
-        public int? LinkID { get; set; } = null;
-        public int? ReportID { get; set; } = null;
-        public int? ReminderID { get; set; } = null;
-        public int? BatchID { get; set; } = null;
-        public int? UserID { get; set; } = null;
-        public int? LoginID { get; set; } = null;
-        public int? PermID { get; set; } = null;
-        public int? ConfigID { get; set; } = null;
         */
+        public int FileID { get; set; } = -999;
+        public int LinkID { get; set; } = -999;
+        public int ReportID { get; set; } = -999;
+        public int ReminderID { get; set; } = -999;
+        public int BatchID { get; set; } = -999;
+        public int UserID { get; set; } = -999;
+        public int LoginID { get; set; } = -999;
+        public int PermID { get; set; } = -999;
+        public int ConfigID { get; set; } = -999;
+        /**/
         // stub
     }
 }
