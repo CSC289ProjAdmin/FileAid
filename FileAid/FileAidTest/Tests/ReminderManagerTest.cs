ec6cfddf1a1392a0b375aa82663d1efd5e18fcbe
Detrice S <dee@aol.com>
2018-03-20 13:03:01 -0400

    Revised FileManagerTest and ReminderManagerTest

--- FileAid/FileAidTest/Tests/ReminderManagerTest.cs
+++ FileAid/FileAidTest/Tests/ReminderManagerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using FileAid.Models;
 
 namespace FileAidTest.Tests {
@@ -7,17 +8,59 @@ namespace FileAidTest.Tests {
     public class ReminderManagerTest {
         [TestMethod]
         public void GetRemindersTest() {
-            Assert.AreEqual(1, 2);
+
+            // Add reminder
+            ReminderManager.AddReminder(new List<int> { 1 }, "Test Reminder", DateTime.Now);
+
+            // Get all reminders
+            List<Reminder> remindersList = ReminderManager.GetReminders();
+
+            // Verify there is at least one reminder
+            Assert.IsNotNull(remindersList);
         }
 
         [TestMethod]
         public void GetReminderTest() {
-            Assert.AreEqual(1, 2);
+
+
+            ReminderManager.AddReminder(new List<int> { 1 }, "Test add reminder", DateTime.Now);
+
+            // Get reminders
+            List <Reminder> reminderList= ReminderManager.GetReminders();
+
+            // Store some data for one of the reminders
+            int reminderId = reminderList[0].ReminderID;
+            string linkName = reminderList[0].Name;
+
+            // Get reminder
+            Reminder reminder = ReminderManager.GetReminder(reminderId);
+
+            // Verify the expected reminder is returned
+            Assert.IsNotNull(reminder);
+            Assert.AreEqual(linkName, reminder.Name);
         }
 
         [TestMethod]
         public void AddReminderTest() {
-            Assert.AreEqual(1, 2);
+
+            int reminderCount = 0;
+
+            // Add a default reminder and then get current reminder count
+            ReminderManager.AddReminder(new List<int> { 1 }, "Test add reminder", DateTime.Now);
+            List < Reminder > remindersList = ReminderManager.GetReminders();
+            reminderCount = remindersList.Count;
+
+            // Add reminder without memo
+            ReminderManager.AddReminder(new List<int> { 1 }, "Test add reminder without memo", DateTime.Now);
+
+            // Verify the reminder was added
+            remindersList = ReminderManager.GetReminders();
+            Assert.AreEqual(reminderCount + 1, remindersList.Count);
+
+            //Add reminder with memo
+            ReminderManager.AddReminder(new List<int> { 1 }, "Test reminder with memo", DateTime.Now, "Description for added reminder");
+            remindersList = ReminderManager.GetReminders();
+            Assert.AreEqual(reminderCount + 2, remindersList.Count);
         }
     }
 }
