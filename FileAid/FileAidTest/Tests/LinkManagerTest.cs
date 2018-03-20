8f31b5425d703e550f79a6dadc3adc45315507f6
Detrice S <dee@aol.com>
2018-03-19 21:31:57 -0400

    Created LinkManagerTest

--- FileAid/FileAidTest/Tests/LinkManagerTest.cs
+++ FileAid/FileAidTest/Tests/LinkManagerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using FileAid.Models;
 
 namespace FileAidTest.Tests {
@@ -7,17 +8,56 @@ namespace FileAidTest.Tests {
     public class LinkManagerTest {
         [TestMethod]
         public void GetLinksTest() {
-            Assert.AreEqual(1, 2);
+
+            // Add link
+            LinkManager.AddLink(new List<int> { 1 }, "Test Get Links");
+
+            // Get all links
+            List<FileLink> linkList = LinkManager.GetLinks();
+
+            // Verify there is at least one link
+            Assert.IsNotNull(linkList);
+
         }
 
         [TestMethod]
         public void GetLinkTest() {
-            Assert.AreEqual(1, 2);
+
+            // Add link
+            LinkManager.AddLink(new List<int> { 1 }, "Test Get Link");
+
+            // Get all links
+            List<FileLink> linkList = LinkManager.GetLinks();
+
+            // Verify there is at least one link
+            Assert.IsNotNull(linkList);
+
+            // Store some data for one of the files
+            int linkId = linkList[0].LinkMemoID;
+            string linkMemo = linkList[0].LinkMemo;
+
+            // Get file link by id
+            FileLink link = LinkManager.GetLink(linkId);
+
+            // Verify the expected link was returned
+            Assert.IsNotNull(link);
         }
 
         [TestMethod]
         public void AddLinkTest() {
-            Assert.AreEqual(1, 2);
+
+            int linkCount = 0;
+
+            LinkManager.AddLink(new List<int> { 1 }, "Test Add Link");
+            List<FileLink> linkList = LinkManager.GetLinks();
+            linkCount = linkList.Count;
+
+            // Add Link
+            LinkManager.AddLink(new List<int> { 1 }, "Test Add Link");
+
+            // Verify the link was added
+            linkList = LinkManager.GetLinks();
+            Assert.AreEqual(linkCount + 1, linkList.Count);
         }
     }
 }
