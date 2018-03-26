6f25525c5969a7a349356bbfcfc3aca29548c7c8
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-26 17:22:58 -0400

    Adds Permissions and PermissionsManager BLL and DAL classes

---
+++ FileAid/FileAid/Models/PermissionsManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public static class PermissionsManager {
+        public static Permissions GetPermissionSet(int userID) {
+            Permissions userPerms = DAL.PermissionsManagerDAL.GetPermissionSet(userID);
+            return userPerms;
+        }
+
+        public static bool UpdatePermissionSet(Permissions perms) {
+            bool wasUpdated = DAL.PermissionsManagerDAL.UpdatePermissionSet(perms);
+            return wasUpdated;
+        }
+    }
+}
