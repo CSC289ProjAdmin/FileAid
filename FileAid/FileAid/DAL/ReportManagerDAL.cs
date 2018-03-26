08e4b5c2e0e239a940feca597092ac354d9c0a37
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-26 18:56:31 -0400

    Adds ReportManager BLL and DAL classes

---
+++ FileAid/FileAid/DAL/ReportManagerDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileAid.Models;
+using System.Data.SqlClient;
+
+namespace FileAid.DAL {
+    public static class ReportManagerDAL {
+        public static List<Report> GetReports() {
+            string select = "Select ReportID, sReportName As Name, sDescription As Description " +
+                "From Reports Where dReportDeleted Is Null; ";
+            return Db.ReadQuery<Report>(select);
+        }
+
+        public static Report GetReportByName(string reportName) {
+            List<SqlParameter> args = new List<SqlParameter>();
+            args.Add(new SqlParameter("@Name", reportName));
+            string select = "Select ReportID, sReportName As Name, sDescription As Description " +
+                "From Reports Where dReportDeleted Is Null As sReportName = @Name; ";
+            List<Report> results = Db.ReadQuery<Report>(select, args.ToArray());
+            if (results != null) {
+                return results[0];
+            } else {
+                return null;
+            }
+        }
+    }
+}
---
+++ FileAid/FileAid/Models/ReportManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAid.Models {
+    public static class ReportManager {
+        public static List<Report> GetReports() {
+            List<Report> allReports = DAL.ReportManagerDAL.GetReports();
+            return allReports;
+        }
+
+        public static Report GetReportByName(string reportName) {
+            Report specific = DAL.ReportManagerDAL.GetReportByName(reportName);
+            return specific;
+        }
+    }
+}
