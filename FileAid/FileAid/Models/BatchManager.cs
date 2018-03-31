c2bc492fb929ba79bd6b744d7c575f743f026f8f
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-03-31 17:24:28 -0400

    Adds stub logic to Scan method for updating and adding database files

--- FileAid/FileAid/Models/BatchManager.cs
+++ FileAid/FileAid/Models/BatchManager.cs
@@ -3,11 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileAid.Models {
     public static class BatchManager {
         public static void Scan() {
-            // stub
+            List<string> officeFileExtensions = new List<string> { "*.doc", "*.xls", "*.ppt" };
+            List<string> searchPaths = new List<string>();
+            Dictionary<string, int> foundFiles = new Dictionary<string, int>();
+            Dictionary<string, int> notFoundFiles = new Dictionary<string, int>();
+
+            List<TrackedFile> allFiles = FileManager.GetFiles();
+            if (allFiles != null) {
+                // For each file in database
+                foreach (var file in allFiles) {
+                    // Add path to unique list to search for new files later
+                    string path = file.FilePath.ToUpper();
+                    bool pathAlreadyAdded = searchPaths.Contains(path);
+                    if (!pathAlreadyAdded) {
+                        searchPaths.Add(path);
+                    }
+
+                    // Create string from filename & extension
+                    string nameWithExt = file.Filename + '.' + file.FileExtension;
+
+                    // Locate file
+                    bool wasFound = false; // TODO: search for file
+
+                    if (wasFound) {
+                        // If being tracked, compare file info & update
+                        bool isTracked = (file.TrackingDisabledOn == new DateTime());
+                        if (isTracked) {
+                            // Compare file info & update
+                        }
+                        // If not being tracked, don't update
+
+                        // Add file to "Handled" dictionary (key = string, value = fileID)
+                    } else { // Add to "Not Found" dictionary (key = string, value = fileID)
+                    }
+                }
+            }
+
+            // Add other desired search paths (e.g. common or specific folders)
+
+            // For each unique path
+            foreach (var path in searchPaths) {
+                // Enumerate files in path with MS Office extensions
+                foreach (var ext in officeFileExtensions) {
+                    try {
+                        var filesInFolder = Directory.EnumerateFiles(path, ext);
+                        // For each file in path
+                        foreach (var file in filesInFolder) {
+                            // If file in "Handled" dictionary, do nothing
+                            bool alreadyHandled = (foundFiles.ContainsKey(file.ToUpper()));
+                            if (!alreadyHandled) {
+                                // If file in "Not Found" dictionary
+                                bool wasMoved = (notFoundFiles.ContainsKey(file.ToUpper()));
+                                if (wasMoved) {
+                                    // Update path, move to "Handled"
+                                    // Update history if file is being tracked
+                                } else { // Add new file to database
+                                }
+                            }
+                        }
+                    } catch (DirectoryNotFoundException) {
+                        // Swallow for now
+                    }
+                }
+            }
+            // For each file left in "Not Found" dictionary
+            foreach (var pair in notFoundFiles) {
+                // Stop tracking
+            }
         }
 
         private static List<string> GetActiveFiles() {
