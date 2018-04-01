df96498144cb7e2863de3bd8abec53fe5f71c5a2
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-04-01 17:52:42 -0400

    Implements database Backup method using straightforward, hard-coded logic.

--- FileAid/FileAid/DAL/DbManagerDAL.cs
+++ FileAid/FileAid/DAL/DbManagerDAL.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FileAid.Models;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace FileAid.DAL {
     public static class DbManagerDAL {
@@ -12,7 +14,35 @@ namespace FileAid.DAL {
         }
 
         public static void Backup(string destName) {
-            // stub
+            string master_ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Database=Master;Integrated Security=True;Connect Timeout=30;";
+
+            using (SqlConnection masterdbConn = new SqlConnection()) {
+                masterdbConn.ConnectionString = master_ConnectionString;
+                masterdbConn.Open();
+
+                using (SqlCommand multiuser_rollback_dbcomm = new SqlCommand()) {
+                    multiuser_rollback_dbcomm.Connection = masterdbConn;
+                    multiuser_rollback_dbcomm.CommandText = @"ALTER DATABASE FileAidDB SET MULTI_USER WITH ROLLBACK IMMEDIATE";
+
+                    multiuser_rollback_dbcomm.ExecuteNonQuery();
+                }
+                masterdbConn.Close();
+            }
+
+            SqlConnection.ClearAllPools();
+
+            using (SqlConnection backupConn = new SqlConnection()) {
+                backupConn.ConnectionString = 
+                    ConfigurationManager.ConnectionStrings["FileAidDBConnectionString"].ConnectionString; ;
+                backupConn.Open();
+
+                using (SqlCommand backupcomm = new SqlCommand()) {
+                    backupcomm.Connection = backupConn;
+                    backupcomm.CommandText = @"BACKUP DATABASE FileAidDB TO DISK='c:\FileAidDB.bak'";
+                    backupcomm.ExecuteNonQuery();
+                }
+                backupConn.Close();
+            }
         }
 
         public static void Restore(string srcName) {
