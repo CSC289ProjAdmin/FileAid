63b294dd95832d00691687474984568bf365c4b1
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-04-08 11:56:01 -0400

    Implements Load event handler for FormFileReports ListView to fill with report details

--- FileAid/FileAid/GUI/FormFileAidReports.cs
+++ FileAid/FileAid/GUI/FormFileAidReports.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FileAid.GUI
 {
@@ -20,6 +21,22 @@ namespace FileAid.GUI
         private void FormFileAidReports_Load(object sender, EventArgs e)
         {
             ReporttoolTip.SetToolTip(btnRunReport, "Run report");
+            try {
+                List<Models.Report> allReports = Models.ReportManager.GetReports();
+                if (allReports == null) {
+                    Models.Messenger.Show("Unable to load report details.");
+                    return;
+                }
+                foreach (var rpt in allReports) {
+                    string[] reportDetails = new string[2];
+                    reportDetails[0] = rpt.Name;
+                    reportDetails[1] = rpt.Description;
+                    ListViewItem row = new ListViewItem(reportDetails);
+                    ReportslistView.Items.Add(row);
+                }
+            } catch (SqlException) {
+                Models.Messenger.Show("Could not communicate with database.");
+            }
         }
     }
 }
