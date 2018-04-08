db78308d12afac4e3c32f771611401b25d998473
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-04-08 13:03:01 -0400

    Implements Load event handler for FormFileAidFileLinks to show link details

--- FileAid/FileAid/GUI/FormFileAidFileLinks.Designer.cs
+++ FileAid/FileAid/GUI/FormFileAidFileLinks.Designer.cs
@@ -29,62 +29,58 @@
         private void InitializeComponent()
         {
             this.FileLinkslistView = new System.Windows.Forms.ListView();
-            this.lblMainInfo = new System.Windows.Forms.Label();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblMainInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FileLinkslistView
             // 
             this.FileLinkslistView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
             this.FileLinkslistView.FullRowSelect = true;
             this.FileLinkslistView.GridLines = true;
-            this.FileLinkslistView.Location = new System.Drawing.Point(83, 87);
+            this.FileLinkslistView.Location = new System.Drawing.Point(55, 57);
+            this.FileLinkslistView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.FileLinkslistView.Name = "FileLinkslistView";
-            this.FileLinkslistView.Size = new System.Drawing.Size(826, 447);
+            this.FileLinkslistView.Size = new System.Drawing.Size(552, 292);
             this.FileLinkslistView.TabIndex = 0;
             this.FileLinkslistView.UseCompatibleStateImageBehavior = false;
             this.FileLinkslistView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Description";
+            this.columnHeader2.Width = 485;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "File Count";
             // 
             // lblMainInfo
             // 
             this.lblMainInfo.AutoSize = true;
             this.lblMainInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMainInfo.ForeColor = System.Drawing.Color.DarkOliveGreen;
-            this.lblMainInfo.Location = new System.Drawing.Point(155, 23);
+            this.lblMainInfo.Location = new System.Drawing.Point(103, 15);
+            this.lblMainInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMainInfo.Name = "lblMainInfo";
-            this.lblMainInfo.Size = new System.Drawing.Size(607, 37);
+            this.lblMainInfo.Size = new System.Drawing.Size(426, 26);
             this.lblMainInfo.TabIndex = 2;
             this.lblMainInfo.Text = "Welcome to FileAid File Links Window.";
             // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Name";
-            this.columnHeader1.Width = 194;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Description";
-            this.columnHeader2.Width = 229;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "File Count";
-            this.columnHeader3.Width = 273;
-            // 
             // FormFileAidFileLinks
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(975, 607);
+            this.ClientSize = new System.Drawing.Size(650, 395);
             this.Controls.Add(this.lblMainInfo);
             this.Controls.Add(this.FileLinkslistView);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormFileAidFileLinks";
             this.Text = "FileAid File Links";
+            this.Load += new System.EventHandler(this.FormFileAidFileLinks_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,7 +90,6 @@
 
         private System.Windows.Forms.ListView FileLinkslistView;
         private System.Windows.Forms.Label lblMainInfo;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
     }
--- FileAid/FileAid/GUI/FormFileAidFileLinks.cs
+++ FileAid/FileAid/GUI/FormFileAidFileLinks.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FileAid.GUI
 {
@@ -15,6 +16,27 @@ namespace FileAid.GUI
         public FormFileAidFileLinks()
         {
             InitializeComponent();
+        }
+
+        private void FormFileAidFileLinks_Load(object sender, EventArgs e) {
+            try {
+                List<Models.FileLink> allLinks = Models.LinkManager.GetLinks();
+                if (allLinks == null) {
+                    // No links in system yet.
+                    return;
+                }
+                foreach (var link in allLinks) {
+                    string[] remDetails = new string[2];
+                    remDetails[0] = link.LinkMemo;
+                    remDetails[1] = link.GetFiles().Count.ToString();
+                    ListViewItem row = new ListViewItem(remDetails);
+                    FileLinkslistView.Items.Add(row);
+                }
+            }
+            catch (SqlException) {
+                Models.Messenger.Show("Could not communicate with database.");
+            }
+
         }
     }
 }
