57ae782f13c69283512347b1c2bebeeebdfd2f1f
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-04-09 14:47:24 -0400

    Adds event logging and listview refreshing after database events

--- FileAid/FileAid/GUI/FormFileAidDbMgmt.cs
+++ FileAid/FileAid/GUI/FormFileAidDbMgmt.cs
@@ -25,6 +25,10 @@ namespace FileAid.GUI
             DBMantoolTip.SetToolTip(btnReset, "Reset the Database");
             DBMantoolTip.SetToolTip(btnDbPrintRepo, "Print the report");
 
+            FillListView();
+        }
+
+        private void FillListView() {
             try {
                 List<Models.Event> allEvents = Models.EventManager.GetEvents();
                 if (allEvents == null) {
@@ -38,7 +42,8 @@ namespace FileAid.GUI
                 };
                 var dbEvents = from ev in allEvents
                                where dbEventIDs.Contains(ev.EventTypeID)
-                               select ev;                 
+                               select ev;
+                DBManlistView.Items.Clear();
                 foreach (var ev in dbEvents) {
                     string[] evDetails = new string[3];
                     evDetails[0] = ev.EventID.ToString();
@@ -59,6 +64,12 @@ namespace FileAid.GUI
             if (dlg.ShowDialog() == DialogResult.OK) {
                 bool wasBackedUp = Models.DbManager.Backup(dlg.SelectedPath);
                 Models.Messenger.Show(wasBackedUp ? "Backup successful." : "Backup failed.");
+                bool wasLogged = LogBackupEvent(dlg.SelectedPath);
+                if (wasLogged) {
+                    FillListView(); // Refresh event list
+                } else {
+                    Models.Messenger.Show("Failed to log backup event.", "FileAid");
+                }
             }
         }
 
@@ -69,15 +80,23 @@ namespace FileAid.GUI
             if (dlg.ShowDialog() == DialogResult.OK) {
                 bool wasRestored = Models.DbManager.Restore(dlg.FileName);
                 Models.Messenger.Show(wasRestored ? "Restore successful." : "Restore failed.");
+                bool wasLogged = LogRestoreEvent(System.IO.Path.GetFileName(dlg.FileName));
+                if (wasLogged) {
+                    FillListView(); // Refresh event list
+                } else {
+                    Models.Messenger.Show("Failed to log restore event.", "FileAid");
+                }
             }
         }
 
         private void btnReset_Click(object sender, EventArgs e) {
             // Prompt twice before resetting
-            string firstPrompt = "Resetting the database will clear all data, revert FileAid to its initial state,\n"
-                + "and cause FileAid to shutdown.\n\n"
+            string firstPrompt = "Resetting the database will:\n"
+                + "1) Clear all data\n"
+                + "2) Revert FileAid to its initial state\n"
+                + "3) Cause FileAid to shutdown.\n\n"
                 + "Are you sure you want to reset?";
-            string secondPrompt = "Resetting the database is final and cannot be undone.\n"
+            string secondPrompt = "Resetting the database is final and cannot be undone.\n\n"
                 + "Are you absolutely sure you want to reset?";
             string caption = "Reset Database";
             DialogResult dr = Models.Messenger.ShowYesNo(firstPrompt, caption);
@@ -89,11 +108,40 @@ namespace FileAid.GUI
 
             bool wasReset = Models.DbManager.Reset();
             if (wasReset) {
-                Models.Messenger.Show("Database has been reset.", caption);
+                bool wasLogged = LogResetEvent();
+                if (!wasLogged) Models.Messenger.Show("Failed to log reset event.", "FileAid");
+                Models.Messenger.Show("Database has been reset.\n\nFileAid will now shut down.", caption);
                 Application.Exit();
             } else {
                 Models.Messenger.Show("Failed to reset database.", caption);
             }
+        }
+
+        private bool LogBackupEvent(string path) {
+            Models.Event ev = new Models.Event();
+            ev.EventTypeID = Models.EventTypes.DbBackedUp;
+            ev.OccurredOn = DateTime.Now;
+            ev.Description = $"Database backed up to {path}";
+            bool wasLogged = Models.Logger.Log(ev);
+            return wasLogged;
+        }
+
+        private bool LogRestoreEvent(string filename) {
+            Models.Event ev = new Models.Event();
+            ev.EventTypeID = Models.EventTypes.DbRestored;
+            ev.OccurredOn = DateTime.Now;
+            ev.Description = $"Database restored from {filename}";
+            bool wasLogged = Models.Logger.Log(ev);
+            return wasLogged;
+        }
+
+        private bool LogResetEvent() {
+            Models.Event ev = new Models.Event();
+            ev.EventTypeID = Models.EventTypes.DbReset;
+            ev.OccurredOn = DateTime.Now;
+            ev.Description = $"Database reset to initial state";
+            bool wasLogged = Models.Logger.Log(ev);
+            return wasLogged;
         }
     }
 }
