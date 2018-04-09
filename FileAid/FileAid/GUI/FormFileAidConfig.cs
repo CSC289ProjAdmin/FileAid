7d4595c83aca97bdb151d5eb4e9ad82800c510de
ASHOK\Ashok <ashokmnr160@gmail.com>
2018-04-08 22:16:02 -0400

    FileAidProgram

--- FileAid/FileAid/GUI/FormFileAidConfig.cs
+++ FileAid/FileAid/GUI/FormFileAidConfig.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace FileAid.GUI
@@ -19,13 +20,67 @@ namespace FileAid.GUI
 
         private void FileAidConfig_Load(object sender, EventArgs e)
         {
-            ConfigtoolTip.SetToolTip(PeriodicdateTimePicker, "Select Periodic Update Date");
-            ConfigtoolTip.SetToolTip(btnPeriodicUpdate, "Periodic Update");
+            ConfigtoolTip.SetToolTip(PeriodicnumericUpDown, "Select Periodic Update Time");
             ConfigtoolTip.SetToolTip(ProInactivecheckBox, "Show inactive files");
             ConfigtoolTip.SetToolTip(txtProgramPath, "Enter folder paths");
             ConfigtoolTip.SetToolTip(btnBrowse, "Browse files");
             ConfigtoolTip.SetToolTip(btnProSaveChanges, "Save Changes");
             ConfigtoolTip.SetToolTip(btnProCancel, "Cancel changes");
+
+            try
+            {
+                Models.Configs getConfig = Models.ConfigManager.GetConfigs();
+
+                if(getConfig == null)
+                {
+                    MessageBox.Show("Unable to load Program setting");
+                }
+
+                ProInactivecheckBox.Checked = getConfig.ShowInactive;
+                PeriodicnumericUpDown.Value = getConfig.UpdateTimerInMinutes;
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Database connection failed");
+            }
+        }
+
+        private void btnProSaveChanges_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Models.Configs getConfig = Models.ConfigManager.GetConfigs();
+
+                if (getConfig == null)
+                {
+                    MessageBox.Show("Unable to load Program setting");
+                }
+
+                getConfig.ShowInactive = ProInactivecheckBox.Checked;
+                getConfig.UpdateTimerInMinutes = (int)PeriodicnumericUpDown.Value;
+
+                Models.Configs wasUpdate = Models.ConfigManager.UpdateConfigs(getConfig);
+                if (wasUpdate != null)
+                {
+                    DialogResult = DialogResult.OK;
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to save changes");
+                    DialogResult = DialogResult.None;
+                }
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Database connection failed");
+            }
+
+        }
+
+        private void btnProCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
