177a0c80acdd6358de1f76a179cb813a682db9de
Detrice S <dee@aol.com>
2018-04-09 22:54:43 -0400

    Creates EventManagerTest and LoginManagerTest files

---
+++ FileAid/FileAidTest/Tests/EventManagerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAidTest.Tests
+{
+    [TestClass]
+    public class EventManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
