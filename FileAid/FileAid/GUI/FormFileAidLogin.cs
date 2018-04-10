6790a9a80a4e607193794abf5e6647007e18a743
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-04-10 11:17:31 -0400

    Adds event logging to Login handler

--- FileAid/FileAid/GUI/FormFileAidLogin.cs
+++ FileAid/FileAid/GUI/FormFileAidLogin.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FileAid.Models;
+using System.Data.SqlClient;
 
 namespace FileAid.GUI
 {
@@ -24,64 +25,74 @@ namespace FileAid.GUI
 
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)) {
                 lblErrorMsg.Text = "Username and password are both required.";
+                txtUserName.Focus();
                 return;
             }
 
-            User u = UserService.Find(username);
-            if (u == null) {
-                lblErrorMsg.Text = "Invalid credentials.";
-                txtUserName.Focus();
-                // TODO?: Log unauthorized access attempt?
-                return;
-            }
+            try {
+                User u = UserService.Find(username);
+                if (u == null) {
+                    lblErrorMsg.Text = "Invalid credentials.";
+                    txtUserName.Focus();
+                    // TODO?: Log unauthorized access attempt?
+                    return;
+                }
 
-            bool passMatches = UserService.VerifyCredentials(u, password);
-            if (!passMatches) {
-                lblErrorMsg.Text = "Invalid credentials.";
-                txtUserName.Focus();
-                LogBadCreds(u);
-                bool isAdmin = (u.Username == "Admin");
-                bool isLocked = UserService.IsLockedOut(u);
-                bool isDisable = UserService.IsDisabled(u);
-                if (!isAdmin && !isLocked && !isDisable) { // Never lock out Admin or disabled accounts
-                    UserService.IncrementFailures(u);
-                    LogFailureIncrement(u);
-                    bool needsLocking = (u.LoginFailures >= 3);
-                    if (needsLocking) {
-                        bool wasLocked = UserService.LockOut(u);
-                        if (wasLocked) {
-                            Messenger.Show("Account locked out for repeated failed logins.", "Account Locked");
-                            DateTime unlocksAt = u.LockedOutOn + TimeSpan.FromHours(1);
-                            lblErrorMsg.Text = $"Account is locked out until {unlocksAt.ToString()}.";
-                            // TODO: Log lockout event
+                bool passMatches = UserService.VerifyCredentials(u, password);
+                if (!passMatches) {
+                    lblErrorMsg.Text = "Invalid credentials.";
+                    txtUserName.Focus();
+                    LogBadCreds(u);
+                    bool isAdmin = (u.Username == "Admin");
+                    bool isLocked = UserService.IsLockedOut(u);
+                    bool isDisable = UserService.IsDisabled(u);
+                    if (!isAdmin && !isLocked && !isDisable) { // Never lock out Admin or disabled accounts
+                        bool wasIncremented = UserService.IncrementFailures(u);
+                        if (wasIncremented) LogFailureIncrement(u);
+                        bool needsLocking = (u.LoginFailures >= 3);
+                        if (needsLocking) {
+                            bool wasLocked = UserService.LockOut(u);
+                            if (wasLocked) {
+                                Messenger.Show("Account locked out for repeated failed logins.", "Account Locked");
+                                DateTime unlocksAt = u.LockedOutOn + TimeSpan.FromHours(1);
+                                lblErrorMsg.Text = $"Account is locked out until {unlocksAt.ToString()}.";
+                                LogLockout(u);
+                            }
                         }
                     }
+                    return;
                 }
-                return;
-            }
 
-            bool isDisabled = UserService.IsDisabled(u);
-            if (isDisabled) {
-                lblErrorMsg.Text = "Account is disabled and cannot log in.";
-                txtUserName.Focus();
-                // TODO: Log failed login due to disabled account
-                return;
-            }
+                bool isDisabled = UserService.IsDisabled(u);
+                if (isDisabled) {
+                    lblErrorMsg.Text = "Account is disabled and cannot log in.";
+                    txtUserName.Focus();
+                    string reason = $"Access denied to {u.Username} because account is disabled.";
+                    LogAccessDenied(u, reason);
+                    return;
+                }
 
-            bool isLockedOut = UserService.IsLockedOut(u);
-            if (isLockedOut) {
-                DateTime unlocksAt = u.LockedOutOn + TimeSpan.FromHours(1);
-                lblErrorMsg.Text = $"Account is locked out until {unlocksAt.ToString()}.";
-                txtUserName.Focus();
-                // TODO: Log failed login due to lockout
-                return;
-            }
+                bool isLockedOut = UserService.IsLockedOut(u);
+                if (isLockedOut) {
+                    DateTime unlocksAt = u.LockedOutOn + TimeSpan.FromHours(1);
+                    lblErrorMsg.Text = $"Account is locked out until {unlocksAt.ToString()}.";
+                    txtUserName.Focus();
+                    string reason = $"Access denied to {u.Username} because account is locked out.";
+                    LogAccessDenied(u, reason);
+                    return;
+                }
 
-            // Account is good otherwise.
-            // TODO: clear account (Unlock, ClearFailures)
-            // Calling form is responsible for getting account & rights.
-            DialogResult = DialogResult.OK;
-            Close();
+                // Account is good otherwise. Reset failures and lockout datetime.
+                UserService.ClearFailures(u);
+                UserService.Unlock(u);
+                LogSuccessfulLogin(u);
+                // Calling form is responsible for getting account & rights.
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (SqlException) {
+                Messenger.ShowDbMsg();
+            }
         }
 
         private void LogBadCreds(User u) {
@@ -101,6 +112,33 @@ namespace FileAid.GUI
             ev.Description = $"Login failure count for {u.Username} incremented.";
             ev.Initial = (u.LoginFailures - 1).ToString();
             ev.New = u.LoginFailures.ToString();
+            Logger.Log(ev);
+        }
+
+        private void LogLockout(User u) {
+            Event ev = new Event();
+            ev.EventTypeID = EventTypes.AccountLockedOut;
+            ev.OccurredOn = DateTime.Now;
+            ev.UserID = u.UserID;
+            ev.Description = $"{u.Username} locked out due to repeated login failures.";
+            Logger.Log(ev);
+        }
+
+        private void LogAccessDenied(User u, string reason) {
+            Event ev = new Event();
+            ev.EventTypeID = EventTypes.LoginFailure;
+            ev.OccurredOn = DateTime.Now;
+            ev.UserID = u.UserID;
+            ev.Description = reason;
+            Logger.Log(ev);
+        }
+
+        private void LogSuccessfulLogin(User u) {
+            Event ev = new Event();
+            ev.EventTypeID = EventTypes.LoginSuccess;
+            ev.OccurredOn = DateTime.Now;
+            ev.UserID = u.UserID;
+            ev.Description = $"{u.Username} logged in successfully.";
             Logger.Log(ev);
         }
 
